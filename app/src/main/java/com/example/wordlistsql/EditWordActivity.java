dad197a9663bf20bb8fe83201dc8f24f135ca0b5
hedilnassraoui <134066709+hedilnassraoui@users.noreply.github.com>
2023-12-16 20:51:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wordlistsql/EditWordActivity.java
@@ -0,0 +1,53 @@
+package com.example.wordlistsql;
+
+import static com.example.wordlistsql.R.id.edit_word;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class EditWordActivity extends AppCompatActivity {
+
+    private static final String TAG = EditWordActivity.class.getSimpleName();
+
+    private static final int NO_ID = -99;
+    private static final String NO_WORD = "";
+
+    private EditText mEditWordView;
+
+    public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
+
+    int mId = MainActivity.WORD_ADD;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_word);
+
+        mEditWordView = findViewById(edit_word);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            int id = extras.getInt(WordListAdapter.EXTRA_ID, NO_ID);
+            String word = extras.getString(WordListAdapter.EXTRA_WORD, NO_WORD);
+            if ((id != NO_ID) && (word != NO_WORD)) {
+                mId = id;
+                mEditWordView.setText(word);
+            }
+        }
+    }
+
+
+    public void returnReply(View view) {
+        String word = mEditWordView.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, word);
+        replyIntent.putExtra(WordListAdapter.EXTRA_ID, mId);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/wordlistsql/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.wordlistsql;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final int WORD_EDIT = 1;
+    public static final int WORD_ADD = -1;
+
+    private WordListOpenHelper mDB;
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private int mLastPosition;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDB = new WordListOpenHelper(this);
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+
+        mAdapter = new WordListAdapter(this, /* mDB.getAllEntries(),*/ mDB);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getBaseContext(), EditWordActivity.class);
+                startActivityForResult(intent, WORD_EDIT);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == WORD_EDIT) {
+            if (resultCode == RESULT_OK) {
+                String word = data.getStringExtra(EditWordActivity.EXTRA_REPLY);
+
+                if (!TextUtils.isEmpty(word)) {
+                    int id = data.getIntExtra(WordListAdapter.EXTRA_ID, -99);
+
+                    if (id == WORD_ADD) {
+                        mDB.insert(word);
+                    } else if (id >= 0) {
+                        mDB.update(id, word);
+                    }
+
+                    mAdapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            R.string.empty_not_saved,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
